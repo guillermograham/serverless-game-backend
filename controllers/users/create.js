61da8c31df1c3618cb37e676ac13e562f9cdcbd6
William Graham <w.m.graham24@gmail.com>
2019-02-04 22:34:01 +0000

    Added db config.

--- controllers/users/create.js
+++ controllers/users/create.js
@@ -1,17 +1,5 @@
-const AWS         = require('aws-sdk');
+const dynamoDb    = require('../../config/db');
 const USERS_TABLE = process.env.USERS_TABLE;
-const IS_OFFLINE  = process.env.IS_OFFLINE;
-
-let dynamoDb;
-
-if (IS_OFFLINE === 'true') {
-  dynamoDb = new AWS.DynamoDB.DocumentClient({
-    region: 'localhost',
-    endpoint: 'http://localhost:8000'
-  });
-} else {
-  dynamoDb = new AWS.DynamoDB.DocumentClient();
-}
 
 module.exports = (req, res) => {
   const { userId, name } = req.body;
